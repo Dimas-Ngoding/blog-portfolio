e21f3d41aaddfaddfbb015de063eb2c216003a34
kodejalaid <70847887+kodejalaid@users.noreply.github.com>
2021-01-18 19:09:04 +0700

    BackToTop Button

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ var typed = new Typed(".type-target", {
   backDelay: 1000,
   backSpeed: 50,
 });
+
+window.onscroll = () => {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    document.getElementById("backToTop").style.display = "block";
+  } else {
+    document.getElementById("backToTop").style.display = "none";
+  }
+};
+
+document.getElementById("backToTop").addEventListener("click", function () {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
+});
