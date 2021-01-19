aee7008ffec891703a6bcfc6b37e6f4016bbd31c
kodejalaid <70847887+kodejalaid@users.noreply.github.com>
2021-01-19 12:02:31 +0700

    Footer Update

--- js/script.js
+++ js/script.js
@@ -27,3 +27,18 @@ document.getElementById("backToTop").addEventListener("click", function () {
   document.body.scrollTop = 0; // Safari
   document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
 });
+
+document.body.onload = function () {
+  let rtClock = new Date();
+  let jam = rtClock.getHours();
+  let menit = rtClock.getMinutes();
+  let detik = rtClock.getSeconds();
+
+  jam = ("0" + jam).slice(-2);
+  menit = ("0" + menit).slice(-2);
+  detik = ("0" + detik).slice(-2);
+
+  document.getElementById("spanJAM").innerHTML =
+    jam + "  :  " + menit + "  :  " + detik;
+  let t = setTimeout(document.body.onload, 500);
+};
